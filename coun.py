82fc43ece30214cdd94308b4625073c99941fdad
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-07-17 04:03:32 -0700

    Update coun.py

--- coun.py
+++ coun.py
@@ -1,10 +1,10 @@
-d,r=map(int,input().split())
+d,n=map(int,input().split())
 arr=[]
 arr=[int(i) for i in input().split()]
 i=0
 sum=0
-while r>0:
+while n>0:
   sum+=arr[i]
-  r=r-1
+  n=n-1
   i=i+1
 print(sum)
