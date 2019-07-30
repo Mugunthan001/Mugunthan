2ad19a0be9ca8261e3c80accac205266cda104c1
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-07-30 15:45:30 +0530

    Update rightshift.py

--- rightshift.py
+++ rightshift.py
@@ -1,7 +1,7 @@
 #M
-num1,num2=map(int,input().split())
-tem=[]
+n,k=map(int,input().split())
+g=[]
 l=list(map(int,input().split()))
-tem=(l[-num2:]+l[:-num2])
-for i in range(0,len(tem)):
-	print(tem[i],end=" ")
+g=(l[-k:]+l[:-k])
+for i in range(0,len(g)):
+	print(g[i],end=" ")
