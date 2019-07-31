cc20330fcb36d8abd51bd8cc5eabdd6b38ef9330
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-07-31 15:59:15 +0530

    Update and rename revswpluss.py to largelement.py

---
+++ largelement.py
@@ -0,0 +1,6 @@
+#M
+n,k=map(int,input().split())
+m=list(map(int,input().split()))
+s=sorted(m)
+w=s[::-1]
+print(w[k-1])
--- revswpluss.py
+++
@@ -1,8 +0,0 @@
-#M
-s=list(map(str,input().split()))
-x=""
-for i in range(0,len(s)):
-    x=s[i]
-    s[i]=x[::-1]
-for i in range(0,len(s)):
-    print(s[i],end=" ")
