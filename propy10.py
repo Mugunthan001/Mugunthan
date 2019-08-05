a6346ffd5b431fcbc53351190bae809b34888730
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-08-05 14:50:54 +0530

    Update and rename propy10.py to propy11.py

--- propy10.py
+++
@@ -1,9 +0,0 @@
-#M
-numl=int(input())
-nums=[int(o) for o in input().split(" ")]
-res=0
-for p in range(numl):
-      for g in range(p):
-           if(nums[g]<nums[p]):
-                res+=nums[g]
-print(res)
---
+++ propy11.py
@@ -0,0 +1,6 @@
+#M
+num=int(input ())
+if num%2==0:
+	print(int(num/2)*(num-1))
+else:
+	print(num)
