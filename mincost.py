41bf9a07dd5ea1c01214c642301acb98aedbc416
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-08-05 14:18:25 +0530

    Update and rename mincost.py to mincostoftwo.py

--- mincost.py
+++
@@ -1,9 +0,0 @@
-#M
-stng1,stng2=input().split()
-char=abs(len(stng2)-len(stng1))
-for g in range(len(stng1)):
-  if(len(stng2)==1 and stng2[g] in stng1):
-    break
-  if(stng1[g]!=stng2[g]):
-    char=char+1
-print(char)
