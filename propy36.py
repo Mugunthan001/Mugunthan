4177e9dcfbc26e4a4b6d49a947e19a737f094a1f
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-08-14 13:49:42 +0530

    Update propy36.py

--- propy36.py
+++ propy36.py
@@ -1,13 +1,12 @@
 #M
-numl=int(input())
-l=list(map(int,input().split()))
-p=0
-for i in range(numl):
-    for j in range(i+1,numl):
-        for k in range(j+1,numl):
-            if l[i]>l[j] and l[j]>l[k]:
-                p=p+1
-if numl==1:
-    print(1)
-else:
-    print(p)
+import sys,string
+numl = int(input())
+L = [ int(x) for x in input().split()]
+numl = len(L)
+cnt = 0
+for i in range(0,numl-2) :
+    for j in range(i+1, numl-1):
+        for k in range(j+1, numl):
+            if L[i] > L[j] > L[k] :
+                cnt += 1
+print(cnt)
