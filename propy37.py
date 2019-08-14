8d1fe87b673539d73a1fdf16d74e9f0f11f36ce8
Mugunthan001 <52281412+Mugunthan001@users.noreply.github.com>
2019-08-14 13:53:11 +0530

    Update propy37.py

--- propy37.py
+++ propy37.py
@@ -7,7 +7,7 @@ if numl==1 :
     print(1)
     sys.exit()
 cnt = 0
-for i in range(1,n-1) :
+for i in range(1,numl-1) :
     if ((L[i] > L[i-1]) and (L[i] > L[i+1])) or ((L[i] < L[i-1]) and (L[i] < L[i+1])):
         cnt += 1
 print(cnt)
